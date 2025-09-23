1d3fba6022e8e747c2a35f79c10907111b7964d2
Ty Bekiares <tyrone.bekiares@elastic.co>
2025-09-23 13:23:11 -0500

    ty

--- instruqt/scripts/elastic-streams.sh
+++ instruqt/scripts/elastic-streams.sh
@@ -38,27 +38,6 @@ enable_significant_events() {
 }
 retry_command_lin enable_significant_events
 
-# ------------- CACHING
-
-# echo "Disable field caching"
-# disable_field_caching() {
-#     local http_status=$(curl -s -o /dev/null -w "%{http_code}" -X POST "$KIBANA_URL/internal/kibana/settings" \
-#     --header 'Content-Type: application/json' \
-#     --header "kbn-xsrf: true" \
-#     --header "Authorization: Basic $ELASTICSEARCH_AUTH_BASE64" \
-#     --header 'x-elastic-internal-origin: Kibana' \
-#     -d '{"changes":{"data_views:cache_max_age":0}}')
-
-#     if echo $http_status | grep -q '^2'; then
-#         echo "Disabled field caching: $http_status"
-#         return 0
-#     else
-#         echo "Failed to disable field caching. HTTP status: $http_status"
-#         return 1
-#     fi
-# }
-# retry_command_lin disable_field_caching
-
 # ------------- DATAVIEW
 
 echo "/api/data_views/data_view"
